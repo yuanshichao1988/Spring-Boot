4838c2876815a2c62e6723bcd49d6458d836b99f
yuanshichao <yuanshichao_gd@163.com>
2017-03-21 09:57:50 +0800

    pu nimabi

---
+++ src/man/java/Application.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator
+ * Date ： 2017/3/21 9:57
+ * Description :
+ * Since:1.0
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("pu nmb!");
+    }
+}
